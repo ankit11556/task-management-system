72ce98323061a41f131fde5f4f6278ada1eefa13
ankit <saharan442090@gmail.com>
2025-11-24 20:30:26 +0530

    feat: add JWT authentication middleware for protected routes

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./src/config/database.js";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
-import authRoutes from "./src/routes/authRoutes.js"
+import authRoutes from "./src/routes/authRoutes.js";
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Task Management System API Running" });
@@ -18,8 +19,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-connectDb().then(()=>{
-app.listen(PORT, () => {
-   console.log(`server is running at http://localhost:${PORT}`);
-});
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running at http://localhost:${PORT}`);
+  });
 });
---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const protect = async (req, res, next) => {
+  const token =
+    req.cookies.token ||
+    (req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer") &&
+      req.headers.authorization.split("")[1]);
+
+  //check json web token exists & is verified
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized, token missing" });
+  }
+
+  try {
+    //verify token
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    req.user = await User.findById(decoded.userId).select("-password");
+
+    if (!req.user) {
+      return res
+        .status(401)
+        .json({ message: "User not found. Please register" });
+    }
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res
+        .status(401)
+        .json({ message: "Session expired. Please login again" });
+    }
+    return res.status(401).json({ message: "Invalid token. Please login." });
+  }
+};
