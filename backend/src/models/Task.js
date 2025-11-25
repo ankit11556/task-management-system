2b6a52816debb6826f8755520c04fad256069baf
ankit <saharan442090@gmail.com>
2025-11-25 10:35:35 +0530

    style: formatted Task.js (indentation & spacing fixes)

--- backend/src/models/Task.js
+++ backend/src/models/Task.js
@@ -1,32 +1,35 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const taskSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'Task title is required'],
-    trim: true
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Task title is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["todo", "in-progress", "completed"],
+      default: "todo",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    deadline: {
+      type: Date,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", //Reference form User model
+      required: true,
+    },
   },
-  description: {
-    type: String
-  },
-  status: {
-    type: String,
-    enum: ["todo", "in-progress", "completed"],
-    default: "todo",
-  },
-  priority: {
-    type: String,
-    enum: ["low", "medium", "high"],
-    default: "medium",
-  },
-  deadline: {
-    type: Date,
-  },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
-  }
-},{timestamps: true})
+  { timestamps: true }
+);
 
-export default mongoose.model("Task",taskSchema)
+export default mongoose.model("Task", taskSchema);
